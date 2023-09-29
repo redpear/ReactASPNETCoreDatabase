fab5c7feeacdd40fef714ce420ab2a8a1068ee62
Angie <redpear@mac.com>
2023-09-29 16:30:39 -0700

    Add project files.

---
+++ ReactASPNETCoreDatabase/dbo/Tables/Department.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [DepartmentId]   INT            IDENTITY (1, 1) NOT NULL,
+    [DepartmentName] NVARCHAR (500) NULL
+);
+
---
+++ ReactASPNETCoreDatabase/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]    INT            IDENTITY (1, 1) NOT NULL,
+    [EmployeeName]  NVARCHAR (500) NULL,
+    [Department]    NVARCHAR (500) NULL,
+    [DateOfJoining] DATETIME       NULL,
+    [PhotoFileName] NVARCHAR (500) NULL
+);
+
